32f3c8923b511a82b628248c91bfd1f12ea30ce8
TarasKendzor <45481754+TarasKendzor@users.noreply.github.com>
2018-12-24 16:36:51 +0200

    upd

--- js/index.js
+++ js/index.js
@@ -25,6 +25,32 @@ document.getElementById('outwear').onclick = function(event){
     document.getElementById('t-shirt-content-m').style.display ='none';
     document.getElementById('outwear-content-m').style.display = 'block';
 }
-
+ document.getElementById('dress').onclick = function(event){
+    event.preventDefault();
+    document.getElementById('dress').style.border ='2px solid #A3A3A3';
+    document.getElementById('w-t-shirt').style.border='none';
+    document.getElementById('w-outwear').style.border ='none';
+    document.getElementById('dress-content').style.display ='block';
+    document.getElementById('w-t-shirt-content').style.display ='none';
+    document.getElementById('w-outwear-content').style.display = 'none';
+  }  
+document.getElementById('w-t-shirt').onclick = function(event){
+    event.preventDefault();
+    document.getElementById('w-t-shirt').style.border ='2px solid #A3A3A3';
+    document.getElementById('dress').style.border ='none';
+    document.getElementById('w-outwear').style.border ='none';
+    document.getElementById('dress-content').style.display ='none';
+    document.getElementById('w-t-shirt-content').style.display ='block';
+    document.getElementById('w-outwear-content').style.display = 'none';
+  }  
+document.getElementById('w-outwear').onclick = function(event){
+    event.preventDefault();
+    document.getElementById('w-outwear').style.border ='2px solid #A3A3A3';
+    document.getElementById('w-t-shirt').style.border='none';
+    document.getElementById('dress').style.border ='none';
+    document.getElementById('dress-content').style.display ='none';
+    document.getElementById('w-t-shirt-content').style.display ='none';
+    document.getElementById('w-outwear-content').style.display = 'block';
+}
 
 
